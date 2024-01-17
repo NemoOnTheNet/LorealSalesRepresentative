b31da5fc6ad0812485db2b8f49b38b47da03f1dc
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2024-01-17 22:04:55 +0100

    add nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
-
+import Nav from "./components/Nav/Nav";
+import "./App.scss"
 
 function App() {
-  return <Outlet />;
+  return (
+    <main className="main">
+      <Outlet />
+      <Nav />
+    </main>
+  );
 }
 
 export default App;
