ce68fedf7e912c5a0c2fbd3f4012c45092766f8b
SebSamPro <sebastien.samyn.pro@gmail.com>
2024-01-18 17:27:56 +0100

    add cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,7 +1,15 @@
 import "./Cart.scss";
 
 function Cart() {
-  return <h1>Je suis sur Cart</h1>;
+  return (
+    <>
+      <p className="cartContainer">Votre panier :</p>
+      <p className="itemCart">3 x parfums (49,99€) "Youthful de Valentino" = 149,97 €</p>
+      <p className="itemCart">2 x soins du visage (19,99€) "Shiny Locks Shampoo d'Yves Saint Laurent" = 39,98 €</p>
+      <p className="itemCart">5 x maquillages (39,99€) "Luminous Foundation de Lancôme" = 199,95 €</p>
+      <p className="totalCart">Total de votre panier = 389,90 €</p>
+    </>
+  )
 }
 
 export default Cart;
