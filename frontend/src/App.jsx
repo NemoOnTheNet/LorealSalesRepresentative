d27f2d134497fc1a32083e8d0b3741d856039445
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2024-01-18 09:32:58 +0100

    add css for the laptop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.scss";
 import SalesRep from "./components/SalesRep/SalesRep";
+import Nav from "./components/Nav/Nav"
 
 function App() {
   return (
     <main className="app__main">
       <SalesRep />
       <Outlet />
+      <Nav />
     </main>
   );
 }
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -4,16 +4,16 @@ import "./Nav.scss";
 function Nav() {
   return (
     <nav className="nav">
-      <Link className="nav__link" to="/histo">
+      <Link className="nav__link hidden" to="/historique">
         <img
           src="src/assets/icons8-horloge-50.png"
           alt="Horloge signifiant l'historique"
         />
       </Link>
-      <Link className="nav__link" to="/plv">
+      <Link className="nav__link hidden" to="/plv">
         <img src="src/assets/icons8-carnet-50.png" alt="liste des PLV" />
       </Link>
-      <Link className="nav__link" to="/sales">
+      <Link className="nav__link" to="/cart">
         <img
           className="nav__link__img"
           src="src/assets/panier.png"
