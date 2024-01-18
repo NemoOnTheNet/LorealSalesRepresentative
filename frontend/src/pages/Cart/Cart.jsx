2cdebfec7f30b5e35deee96587b7f2b48f82d970
SebSamPro <sebastien.samyn.pro@gmail.com>
2024-01-18 18:27:01 +0100

    add positions about cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,11 +3,13 @@ import "./Cart.scss";
 function Cart() {
   return (
     <>
-      <p className="cartContainer">Votre panier :</p>
-      <p className="itemCart">3 x parfums (49,99€) "Youthful de Valentino" = 149,97 €</p>
-      <p className="itemCart">2 x soins du visage (19,99€) "Shiny Locks Shampoo d'Yves Saint Laurent" = 39,98 €</p>
-      <p className="itemCart">5 x maquillages (39,99€) "Luminous Foundation de Lancôme" = 199,95 €</p>
-      <p className="totalCart">Total de votre panier = 389,90 €</p>
+      <section className="cartContainer">
+        <p className="titleCart">Votre panier :</p>
+        <p className="itemCart1">3 x parfums (49,99€) "Youthful de Valentino" = 149,97 €</p>
+        <p className="itemCart2">2 x soins du visage (19,99€) "Shiny Locks Shampoo d'Yves Saint Laurent" = 39,98 €</p>
+        <p className="itemCart3">5 x maquillages (39,99€) "Luminous Foundation de Lancôme" = 199,95 €</p>
+        <p className="totalCart">Total de votre panier = 389,90 €</p>
+      </section>
     </>
   )
 }
