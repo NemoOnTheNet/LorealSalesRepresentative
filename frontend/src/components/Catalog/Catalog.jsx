5525926b1e5f8f72678126101ac1a00718096fc7
Florence Buchelet <florence.buchelet@gmail.com>
2024-01-18 22:32:40 +0100

    map on catalog

--- frontend/src/components/Catalog/Catalog.jsx
+++ frontend/src/components/Catalog/Catalog.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
-import { useGlobalContext } from "../Context/GlobalContextProvider";
 import "./Catalog.scss";
 
 function Catalog() {
-  const { chooseCity } = useGlobalContext();
-    const city = chooseCity;
-    const [result, setResult] = useState();
+  const city = "Lille";
+  const [result, setResult] = useState();
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/city/${city}`)
+      .then((response) => response.json())
+      .then((data) => setResult(data))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(result);
 
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/city/${city}`)
-          .then((response) => response.json())
-          .then((data) => setResult(data))
-          .catch((error) => console.error(error));
-      }, []);
-      console.log(result);
-
-  return <>{result ? result.map((product) => <p>hi</p>) : Loading}</>;
+  return (
+    <>
+      {result.map((product) => (
+        <article>
+          <h2>{product.name}</h2>
+          <img src={product.picture} />
+        </article>
+      ))}
+    </>
+  );
 }
 
 export default Catalog;
